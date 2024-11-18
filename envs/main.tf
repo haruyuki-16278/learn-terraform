ce80e4aa4af8fd78af7aebaf13e0ee9bb5a2f911
haruyuki_16278 <haruyuki@relicsnow.info>
2024-11-18 19:21:15 +0900

    👍 IAMを作るところまで

--- envs/main.tf
+++ envs/main.tf
@@ -2,3 +2,9 @@ module "dynamodb" {
   prefix = "sample1"
   source = "../modules/dynamodb"
 }
+
+module "iam" {
+  prefix = "sample1"
+  source = "../modules/iam"
+  employee_list_table-arn = module.dynamodb.employee_list_table.arn
+}
