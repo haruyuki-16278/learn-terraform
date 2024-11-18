52c05f3407566f7888e4004a67000b915bf3b2b2
haruyuki_16278 <haruyuki@relicsnow.info>
2024-11-18 19:52:59 +0900

    👍 Lambdaを作成するまで

--- envs/main.tf
+++ envs/main.tf
@@ -8,3 +8,10 @@ module "iam" {
   source = "../modules/iam"
   employee_list_table-arn = module.dynamodb.employee_list_table.arn
 }
+
+module "lambda" {
+  prefix = "sample1"
+  source = "../modules/lambda"
+  employee_list_table-name = module.dynamodb.employee_list_table.name
+  tr_lambda_role-arn = module.iam.tr_lambda_role-arn
+}
